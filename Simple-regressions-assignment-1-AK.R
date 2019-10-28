137e47a8d6388ee9042111153f2a9421ed5592be
AnnaK42 <56561645+AnnaK42@users.noreply.github.com>
2019-10-28 16:10:48 -0400

    Add files via upload

--- Simple-regressions-assignment-1-AK.R
+++ Simple-regressions-assignment-1-AK.R
@@ -53,3 +53,56 @@ ggplot(data = PredVars, aes(y = PredVars$YAnswer,x = x_1,x_2,x_3)) + geom_point(
 fit_x1 <- lm(PredVars$YAnswer ~ x_1)
 fit_x2 <- lm(PredVars$YAnswer ~ x_2)
 fit_x3 <- lm(PredVars$YAnswer ~ x_3)
+
+LM_Fun <- function(beta0,beta1,sigma,x_1,n_obs){
+  PredVars1 <- data.frame("YAnswer" = integer(n_obs))
+  PredVars1 <- mutate(PredVars1,YAnswer = beta_0 + x_1*beta_1 + sigma)
+  ggplot(data = PredVars, aes(y = YAnswer,x = x_1)) + geom_point()
+  output <- PredVars1
+}
+  
+#Was supposed to add lm(PredVars1$YAnswer ~ x_1) to previous function
+
+
+sim_simple_lm <- function(n_obs, beta_0, beta_1, sigma) { x_1 <- as.numeric(scale(rnorm(n_obs))) 
+errors <- rnorm(n_obs, 0, sigma) 
+y <- beta_0 + beta_1 * x_1 + errors 
+fit <- lm(y ~ x_1) 
+  output <- list(n_obs = n_obs, 
+                 beta_0 = beta_0, 
+                 beta_0_hat = fit$coef["(Intercept)"], 
+                 beta_1 = beta_1, 
+                 beta_1_hat = fit$coef["x_1"], 
+                 sigma = sigma, 
+                 sigma_hat = sd(fit$resid)) 
+return(output)
+}
+
+
+set.seed(854)
+sim_simple_lm(10,1,2,10)
+
+
+n_sims <- 1e4 #n_sims = 10000, but in scientific notation
+sim_results <- data.frame(sim = 1:n_sims,
+                          beta_0_hat = NA,
+                          beta_1_hat = NA,
+                          sigma_hat = NA)
+for(this_sim in 1:n_sims) {
+  if(this_sim %% 1000 == 0) {cat("starting simulation", this_sim, "\n")}
+  #do something every 1000th iteration is what Var %% Num == 0 means. Num = 1000 in this case but it could be every 10 or every 50 iterations for example.
+  #If you type in "this_sim %% 1000" you get 0.
+  this_fit <- sim_simple_lm(10,1,2,10)
+  sim_results[this_sim,2:4] <- c(this_fit$beta_0_hat,
+                                 this_fit$beta_1_hat,
+                                 this_fit$sigma_hat)
+}
+summary(sim_results[,2:4])
+
+
+
+this_fit1 <- as.data.frame(this_fit)
+GatheredFit <- gather(this_fit1)
+
+    
+
